6f3d68b17936aa0c657725045dead6c85790233f
L3g7 <contact@l3g7.dev>
2024-12-06 16:15:42 +0100

    Fix MessageModifyEvent not unmodifying forge msgs

--- addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby3/temp/TempMessageModifyHandler.java
+++ addon/game-runner/src/v1_8_9/java/dev/l3g7/griefer_utils/labymod/laby3/temp/TempMessageModifyHandler.java
@@ -10,28 +10,93 @@ package dev.l3g7.griefer_utils.labymod.laby3.temp;
 import dev.l3g7.griefer_utils.core.api.bridges.Bridge.ExclusiveTo;
 import dev.l3g7.griefer_utils.labymod.laby3.bridges.LabyBridgeImpl;
 import net.labymod.utils.manager.TagManager;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.play.server.S02PacketChat;
 import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static dev.l3g7.griefer_utils.core.api.bridges.Bridge.Version.LABY_3;
 import static dev.l3g7.griefer_utils.core.api.bridges.LabyBridge.labyBridge;
 
 @ExclusiveTo(LABY_3)
-@Mixin(value = TagManager.class, remap = false)
 public class TempMessageModifyHandler {
 
-	@Inject(method = "tagComponent", at = @At("RETURN"), cancellable = true)
-	private static void injectTagComponent(Object chatComponent, CallbackInfoReturnable<Object> cir) {
-		IChatComponent original = (IChatComponent) chatComponent;
-		IChatComponent modified = (IChatComponent) cir.getReturnValue();
+	private static final LinkedHashMap<ComponentHash, IChatComponent> unmodifiedChatComponents = new LinkedHashMap<>() {
+		protected boolean removeEldestEntry(Map.Entry<ComponentHash, IChatComponent> eldest) {
+			return size() > 100;
+		}
+	};
 
-		modified = (IChatComponent) ((LabyBridgeImpl) labyBridge).messageModifyConsumer
-			.apply(original, modified);
+	@ExclusiveTo(LABY_3)
+	@Mixin(value = TagManager.class, remap = false)
+	public static class TagManagerMixin {
 
-		cir.setReturnValue(modified);
+		@Unique
+		private static IChatComponent grieferUtils$currentComponent;
+
+		@Inject(method = "tagComponent", at = @At("HEAD"))
+		private static void injectTagComponentHEAD(Object chatComponent, CallbackInfoReturnable<Object> cir) {
+			grieferUtils$currentComponent = (IChatComponent) chatComponent;
+		}
+
+		@Inject(method = "tagComponent", at = @At("RETURN"), cancellable = true)
+		private static void injectTagComponent(Object chatComponent, CallbackInfoReturnable<Object> cir) {
+			cir.setReturnValue(((LabyBridgeImpl) labyBridge).messageModifyConsumer
+				.apply(unmodifiedChatComponents.get(new ComponentHash(grieferUtils$currentComponent)), cir.getReturnValue()));
+		}
+
 	}
 
+	@ExclusiveTo(LABY_3)
+	@Mixin(value = NetHandlerPlayClient.class)
+	public static class NetHandlerPlayClientMixin {
+
+		@Unique
+		private IChatComponent grieferUtils$currentComponent;
+
+		@Inject(method = "handleChat", at = @At("HEAD"))
+		private void injectTagComponent(S02PacketChat packet, CallbackInfo ci) {
+			grieferUtils$currentComponent = packet.getChatComponent();
+		}
+
+		@ModifyArg(method = "handleChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiNewChat;printChatMessage(Lnet/minecraft/util/IChatComponent;)V"))
+		private IChatComponent injectTagComponent(IChatComponent chatComponent) {
+			unmodifiedChatComponents.put(new ComponentHash(chatComponent), grieferUtils$currentComponent);
+			return chatComponent;
+		}
+
+	}
+
+	/**
+	 * Wrapped around IChatComponent to use identity hash codes instead of IChatComponent's hashCode method.
+	 */
+	@ExclusiveTo(LABY_3)
+	private static class ComponentHash {
+
+		private final IChatComponent component;
+
+		public ComponentHash(IChatComponent component) {
+			this.component = component;
+		}
+
+		@Override
+		public int hashCode() {
+			return System.identityHashCode(component);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return obj instanceof ComponentHash ch && component == ch.component;
+		}
+
+	}
 }
